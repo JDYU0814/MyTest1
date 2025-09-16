78f4b6a287a370d76ee410249bf34a2fcb32cfd7
jiade <jiade@CAREY-TECH>
2025-09-16 11:45:15 +0800

    locale change

--- MyTest 1/Program.cs
+++ MyTest 1/Program.cs
@@ -10,7 +10,7 @@ namespace MyTest_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("世界，你好。");
+            Console.WriteLine("世界，你好。。。");
         }
     }
 }
