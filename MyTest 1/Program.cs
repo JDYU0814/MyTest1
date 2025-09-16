1b2ae8c19c76841aecff56c5715019ba0527adcb
jiade <jiade@CAREY-TECH>
2025-09-16 11:41:00 +0800

    en-us to zh-tw

--- MyTest 1/Program.cs
+++ MyTest 1/Program.cs
@@ -10,7 +10,7 @@ namespace MyTest_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("世界，你好");
         }
     }
 }
